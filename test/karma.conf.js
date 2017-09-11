dbcf8829897987cdeea84cd1caab8e8ed1d8173e
Mick Dekkers <mick@blocklevel.nl>
2017-09-11 17:53:35 +0200

    feat: use mocha reporter for tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -68,7 +68,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
     // enable / disable colors in the output (reporters and logs)
     colors: true,
